ba2e9009a0e06501f94ab450a1bf0093ceadfced
BilldoM <williammelvin26@yahoo.com>
2016-03-20 10:53:49 -0400

    added multiples app extreme

--- Multiples App Extreme/ViewController.swift
+++ Multiples App Extreme/ViewController.swift
@@ -9,17 +9,98 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    //Properties
+    var multiplier: Int = 0
+    var runningSum: Int = 0
+    let maxPress = 10
+    var counterCount: Int = 0
+    
+    
+    //Outlets
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var multiplyTextField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var multiplyLabel: UILabel!
+    @IBOutlet weak var multiplyButton: UIButton!
+    
+    @IBAction func onPressMultiplyButton(sender: AnyObject) {
+        let newSum = runningSum + multiplier
+        updateLabel(runningSum, mul: multiplier, newSum: newSum)
+        runningSum += multiplier
+        
+        if gameOver() {
+            reLoadGame()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    
+    @IBAction func pressPlayButton(sender: AnyObject) {
+        
+            counterCount += 1
+            print(counterCount)
+        
+        
+        if multiplyTextField.text != nil && multiplyTextField.text != "" {
+            
+        logo.hidden = true
+        multiplyTextField.hidden = true
+        playButton.hidden = true
+        
+        multiplyLabel.hidden = false
+        multiplyButton.hidden = false
+            
+        resetLabel()
+        multiplier = Int(multiplyTextField.text!)!
+        runningSum = 0
+      
+        }
+        
     }
-
-
+    
+    func reLoadGame() {
+        multiplier = 0
+        multiplyTextField.text = ""
+        multiplyTextField.hidden = false
+        logo.hidden = false
+        playButton.hidden = false
+        multiplyLabel.hidden = true
+        multiplyButton.hidden = true
+        resetLabel()
+    }
+    
+    func resetLabel() {
+        multiplyLabel.text = "Press Add to Add!"
+    }
+    
+   
+    
+    func gameOver() -> Bool {
+        if counterCount == maxPress {
+        return true
+        } else {
+            return false
+        }
+    }
+    
+    
+    func updateLabel(oldSum: Int, mul: Int, newSum: Int) {
+        multiplyLabel.text = "\(oldSum) + \(mul) = \(newSum)"
+    }
+    
+    
+    
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
